0c550057508b99eac89d0a97504918ec8b0d772f
Iris <OCT@OCTmatoMacBook-Pro.local>
2013-11-21 21:02:28 +0800

    make file and long by iris

--- makefile
+++ makefile
@@ -1,12 +1,11 @@
-cc=clang
-cc=gcc
 nas: nas.c nas.tab.c
-	$(cc) -o nas nas.c nas.tab.c
+	gcc -o nas nas.c nas.tab.c
 
 nas.c: nas.l
 	flex -o nas.c nas.l
 
 nas.tab.c: nas.y
 	bison -d nas.y
-cleanas:
+
+cleannas:
 	$(RM) nas.c nas.tab.c nas.tab.h nas
--- nas.tab.h
+++ nas.tab.h
@@ -1,13 +1,14 @@
-/* A Bison parser, made by GNU Bison 3.0.  */
+/* A Bison parser, made by GNU Bison 2.3.  */
 
-/* Bison interface for Yacc-like parsers in C
+/* Skeleton interface for Bison's Yacc-like parsers in C
 
-   Copyright (C) 1984, 1989-1990, 2000-2013 Free Software Foundation, Inc.
+   Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
+   Free Software Foundation, Inc.
 
-   This program is free software: you can redistribute it and/or modify
+   This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
 
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -15,7 +16,9 @@
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.  */
 
 /* As a special exception, you may create a larger work that contains
    part or all of the Bison parser skeleton and distribute that work
@@ -30,78 +33,103 @@
    This special exception was added by the Free Software Foundation in
    version 2.2 of Bison.  */
 
-#ifndef YY_YY_NAS_TAB_H_INCLUDED
-# define YY_YY_NAS_TAB_H_INCLUDED
-/* Debug traces.  */
-#ifndef YYDEBUG
-# define YYDEBUG 0
-#endif
-#if YYDEBUG
-extern int yydebug;
-#endif
-
-/* Token type.  */
+/* Tokens.  */
 #ifndef YYTOKENTYPE
 # define YYTOKENTYPE
-  enum yytokentype
-  {
-    INT = 258,
-    REG = 259,
-    LABEL = 260,
-    PUSH = 261,
-    POP = 262,
-    LT = 263,
-    GT = 264,
-    GE = 265,
-    LE = 266,
-    NE = 267,
-    EQ = 268,
-    STRING = 269,
-    CALL = 270,
-    RET = 271,
-    END = 272,
-    J0 = 273,
-    J1 = 274,
-    JMP = 275,
-    ADD = 276,
-    SUB = 277,
-    MUL = 278,
-    DIV = 279,
-    MOD = 280,
-    NEG = 281,
-    AND = 282,
-    OR = 283,
-    GETI = 284,
-    GETS = 285,
-    GETC = 286,
-    PUTI = 287,
-    PUTS = 288,
-    PUTC = 289,
-    PUTI_ = 290,
-    PUTS_ = 291,
-    PUTC_ = 292
-  };
+   /* Put the tokens into the symbol table, so that GDB and other debuggers
+      know about them.  */
+   enum yytokentype {
+     INT = 258,
+     REG = 259,
+     LABEL = 260,
+     PUSH = 261,
+     POP = 262,
+     LT = 263,
+     GT = 264,
+     GE = 265,
+     LE = 266,
+     NE = 267,
+     EQ = 268,
+     STRING = 269,
+     CALL = 270,
+     RET = 271,
+     END = 272,
+     J0 = 273,
+     J1 = 274,
+     JMP = 275,
+     ADD = 276,
+     SUB = 277,
+     MUL = 278,
+     DIV = 279,
+     MOD = 280,
+     NEG = 281,
+     AND = 282,
+     OR = 283,
+     GETI = 284,
+     GETS = 285,
+     GETC = 286,
+     PUTI = 287,
+     PUTS = 288,
+     PUTC = 289,
+     PUTI_ = 290,
+     PUTS_ = 291,
+     PUTC_ = 292
+   };
 #endif
+/* Tokens.  */
+#define INT 258
+#define REG 259
+#define LABEL 260
+#define PUSH 261
+#define POP 262
+#define LT 263
+#define GT 264
+#define GE 265
+#define LE 266
+#define NE 267
+#define EQ 268
+#define STRING 269
+#define CALL 270
+#define RET 271
+#define END 272
+#define J0 273
+#define J1 274
+#define JMP 275
+#define ADD 276
+#define SUB 277
+#define MUL 278
+#define DIV 279
+#define MOD 280
+#define NEG 281
+#define AND 282
+#define OR 283
+#define GETI 284
+#define GETS 285
+#define GETC 286
+#define PUTI 287
+#define PUTS 288
+#define PUTC 289
+#define PUTI_ 290
+#define PUTS_ 291
+#define PUTC_ 292
 
-/* Value type.  */
+
+
+
 #if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
-typedef union YYSTYPE YYSTYPE;
-union YYSTYPE
+typedef union YYSTYPE
+#line 45 "nas.y"
 {
-#line 45 "nas.y" /* yacc.c:1909  */
-
-  int i;
+  long i;
   char s[500];
-
-#line 97 "nas.tab.h" /* yacc.c:1909  */
-};
-# define YYSTYPE_IS_TRIVIAL 1
+}
+/* Line 1529 of yacc.c.  */
+#line 128 "nas.tab.h"
+	YYSTYPE;
+# define yystype YYSTYPE /* obsolescent; will be withdrawn */
 # define YYSTYPE_IS_DECLARED 1
+# define YYSTYPE_IS_TRIVIAL 1
 #endif
-
 
 extern YYSTYPE yylval;
 
-int yyparse (void);
-
-#endif /* !YY_YY_NAS_TAB_H_INCLUDED  */
