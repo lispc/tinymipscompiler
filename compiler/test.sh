a9fb953b85066507c08bd8d20c3b45f20960c74a
Iris <OCT@OCTmatoMacBook-Pro.local>
2013-11-27 15:33:18 +0800

    update test.sh

--- compiler/test.sh
+++ compiler/test.sh
@@ -1,4 +1,6 @@
-FILES=("nested_for_loop" "nested_while_loop")
+cd ../test
+FILES=($(ls | cut -d'.' -f1 | uniq))
+cd ../compiler
 mkdir error
 make; mv mcc ./error
 make clean
@@ -7,7 +9,7 @@ cd ../test
 for FILE in ${FILES[@]}
 do
   cp $FILE.sc ../compiler/error
-  cp $FILE.out ../compiler/error
+  cp $FILE.sample_out ../compiler/error
 done
 cd ../compiler/error
 touch report
@@ -15,16 +17,16 @@ for FILE in ${FILES[@]}
 do
   ./mcc $FILE.sc > $FILE.as
   ./nas $FILE.as > $FILE.output
-  if diff $FILE.output $FILE.out > $FILE.cmp
+  if diff $FILE.output $FILE.sample_out > $FILE.cmp
   then
     echo -e "\033[32mSuccess: $FILE\033[0m" >> report
-    rm $FILE.sc; rm $FILE.as; rm $FILE.out; rm $FILE.output; rm $FILE.cmp
+    rm $FILE.sc; rm $FILE.as; rm $FILE.sample_out; rm $FILE.output; rm $FILE.cmp
   else
     echo -e "\033[31mFail: $FILE\033[0m" >> report
     mkdir $FILE
     mv $FILE.sc ./$FILE
     mv $FILE.as ./$FILE
-    mv $FILE.out ./$FILE
+    mv $FILE.sample_out ./$FILE
     mv $FILE.output ./$FILE
     mv $FILE.cmp ./$FILE
   fi
